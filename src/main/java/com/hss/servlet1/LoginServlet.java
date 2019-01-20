17b487dcb060af38ce46053e195dd65e16861cdb
hss <hss@hssdeMac-mini.local>
2019-01-20 16:52:05 +0800

    init

---
+++ src/main/java/com/hss/servlet1/LoginServlet.java
@@ -0,0 +1,46 @@
+package com.hss.servlet1;
+
+import com.sun.net.httpserver.HttpsServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (userName.equals("hss") && password.equals("123")){
+//response.getWriter().write("登录成功");
+			/*
+			 * 早期的写法：
+			 * response.setStatus(302);
+			response.setHeader("Location", "login_success.html");*/
+
+            //重定向写法： 重新定位方向  /根目录
+            response.sendRedirect("login_success.html");
+
+
+            //请求转发的写法：
+//			request.getRequestDispatcher("login_success.html").forward(request, response);
+        }else {
+            response.getWriter().write("登录失败");
+
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+
+<form action="login">
+    name:<input type="text" name="username" /><br/>
+    password:<input type="password" name="password" /><br/>
+    <input type="submit" value="提交" />
+</form>
+</html>
